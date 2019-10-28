da511c59f61044639d5625c2db1d840eb5c304e8
Solomon R Pena <solomonraulpena@gmail.com>
2019-10-28 18:35:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Write your code here!
 let newHeader = document.createElement('h1')
 
+// for (let i = 0; i < 3; i++) {
+//   let li = document.createElement('li')
+//   li.innerHTML = (i + 1).toString()
+//   ul.appendChild(li)
+// }

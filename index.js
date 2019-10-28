f138efa2b9aad84bc378a7c74f522497a3c31e35
Solomon R Pena <solomonraulpena@gmail.com>
2019-10-28 18:44:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let newHeader = document.createElement('h1')
 newHeader.id = 'victory'
 
 // let element = document.querySelector("h1#victory");
-newHeader.innerHTML = 'Hello, DOM!'
+newHeader.innerHTML = 'YOUR-NAME is the champion'

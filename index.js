05c1ae52e0473ecc420d194003a2d2ae985ea775
Solomon R Pena <solomonraulpena@gmail.com>
2019-10-28 18:46:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+// let element = document.querySelector("h1#victory");
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+main.remove()

de31254c69b55ea6ef760c4f12c3a363911076bd
Solomon R Pena <solomonraulpena@gmail.com>
2019-10-28 18:37:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code here!
 let newHeader = document.createElement('h1')
-.remove()
+newHeader.remove()
 // for (let i = 0; i < 3; i++) {
 //   let li = document.createElement('li')
 //   li.innerHTML = (i + 1).toString()

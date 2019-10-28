88484020a36f9a23f6224aa6420c98127b4d6212
Solomon R Pena <solomonraulpena@gmail.com>
2019-10-28 18:41:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 let newHeader = document.createElement('h1')
 newHeader.id = 'victory'
-// for (let i = 0; i < 3; i++) {
-//   let li = document.createElement('li')
-//   li.innerHTML = (i + 1).toString()
-//   ul.appendChild(li)
-// }
+
